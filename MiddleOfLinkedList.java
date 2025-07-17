ff1b52652671f05558bfd035da50d19bf80c5941
Bhawik Tanna <btanna@akamai.com>
2025-07-17 10:12:53 +0530

    MiddleOfLinkedList

---
+++ MiddleOfLinkedList.java
@@ -0,0 +1,275 @@
+import java.util.*;
+
+
+public class MiddleOfLinkedList
+{
+    
+    public static class Node{
+        int data;
+        Node next;
+}
+
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+        
+    public int midOfLinkedList(){
+        Node slow = head, fast = head;
+                //fast != tail || fast.next != tail
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }    
+        
+    public int kthElementFromLast(int k){
+        Node slow = head, fast = head;
+        for(int i=0;i<k;i++){
+            fast = fast.next;
+        }
+        
+        while(fast != tail){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow.data;
+    }    
+    
+    public void removeAt(int idx){
+        //This function is wrong at the mmoment
+        if(idx<0 || idx>=size){
+            System.out.println("Invalid arguments");
+        }
+        else if(idx == 0){
+            removeFirst();
+        }
+        else if(idx == size-1){
+            removeLast();
+        }
+        else{
+            Node temp = head;
+            for(int i=0;i<size-2;i++){
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            size--;
+        }
+    }    
+        
+    public void reversePI(){
+        Node prev = null, curr = head;
+        
+        while(curr != null){
+            Node next = curr.next;
+            curr.next = prev;
+            
+            prev = curr;
+            curr = next;
+        }
+        Node temp = head;
+        head = tail;
+        tail = head;
+    }    
+        
+    private Node getNodeAt(int idx){
+        Node temp = head;
+        for(int i=0;i<idx;i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+    
+    public void reverseDI(){
+        int li = 0;
+        int ri = size-1;
+        while(li < ri){
+            Node leftNode = getNodeAt(li);
+            Node rightNode = getNodeAt(ri);
+            int temp = leftNode.data;
+            leftNode.data = rightNode.data;
+            rightNode.data = temp;
+            li++; ri--;
+        }
+    }
+    public void removeLast(){
+        if(size == 0){
+            System.out.println("List is empty");
+        }
+        else if(size == 1){
+            head = tail = null;
+            size--;
+        }
+        else{
+            Node temp = head;
+            for(int i=0;i<size-2;i++){
+                temp = temp.next;
+            }
+            tail = temp;
+            tail.next = null;
+            size--;
+        }
+    } 
+    
+
+    
+    public void addAtIndex(int idx, int value){
+        if(idx<0 || idx > size){
+            System.out.println("Invalid arguments");
+        }
+        else if(idx == 0){
+            addFirst(value);
+        }
+        else if(idx == size){
+            addLast(value);
+        }
+        else{
+            Node temp = new Node();
+            temp.data = value;
+            Node newPtr = head;
+            Node prevPtr = head;
+            for(int i=0;i<idx;i++){
+                prevPtr = newPtr;
+                newPtr = newPtr.next;
+            }
+            temp.next = newPtr;
+            prevPtr.next = temp;
+            size++;
+        }
+    }    
+        
+    public void addFirst(int value){
+        Node temp = new Node();
+        temp.data = value;
+        if(size == 0){
+            head = tail = temp;
+        }
+        else{
+            Node t = head;
+            head = temp;
+            temp.next = t;
+        }
+        size++;
+    }    
+        
+    public int getAt(int idx){
+        if(size == 0){
+            System.out.println("List is empty");
+            return -1;
+        }
+        else if(idx<0 || idx >= size){
+            System.out.println("Invalid arguments");
+            return -1;
+        }
+        else{
+            Node temp = head;
+            for(int i=0;i<idx;i++){
+                temp = temp.next;
+            }
+            return temp.data;
+        }
+    }    
+        
+    void removeFirst(){
+        if(size == 0){
+            System.out.println("List is empty");
+        }
+        else if(size == 1){
+            head = tail = null;
+            size = 0;
+        }
+        else{
+            head = head.next;
+            size--;
+        }
+    }    
+    
+    void sizeofLinkedList(){
+        System.out.println("size of LinkedList: "+this.size);
+    }
+    
+    void display(){
+        Node temp = head;
+        
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    
+    void addLast(int val){
+        Node temp = new Node();
+        temp.data = val;
+        temp.next = null;
+        
+        if(size == 0){
+            head = tail = temp;
+        }
+        else{
+            tail.next = temp;
+            tail = temp;
+        }
+        size++;
+    }
+    
+    
+}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<Integer> lst = new ArrayList<>();
+		while (sc.hasNextInt()) {
+		    int i = sc.nextInt();
+		    lst.add(i);
+		}
+
+		LinkedList l = new LinkedList();
+		for(int i = 0;i<lst.size();i++){
+		    l.addLast(lst.get(i));
+		}
+		
+// 		System.out.println(l.getAt(0));
+// 		System.out.println(l.getAt(1));
+	    l.display();
+	    System.out.println(l.midOfLinkedList());
+	    l.addLast(11);
+	    System.out.println(l.midOfLinkedList());
+	   // System.out.println(l.kthElementFromLast(2));
+	   // System.out.println(l.kthElementFromLast(3));
+	   // System.out.println(l.kthElementFromLast(10));
+	   // l.addFirst(-11);
+	   // l.display();
+	   // l.addAtIndex(1, -12);
+	   // l.display();
+	   // l.addAtIndex(0, -13);
+	   // l.display();
+	   // l.addAtIndex(7, -13);
+	   // l.display();
+	   // l.addAtIndex(9, -33);
+	   // l.display();
+	   // l.addAtIndex(10, -13);
+	   // l.display();
+	   // l.addAtIndex(100, -13);
+	   // l.display();
+	    
+	    
+	   // l.reverseDI();
+	   // l.display();
+	    
+	   // l.reverseDI();
+	   // l.display();
+	    
+	   // l.reversePI();
+	   // l.display();
+	    
+	   // l.removeAt(0);
+	   // l.display();
+	    
+	   // l.removeAt(1);
+	   // l.display();
+	    
+	   // l.removeAt(5);
+	   // l.display();
+	}
+}
